f346c96fba3ca36e8cf13b839c68500ee62f310a
LachezarLalov <113022668+LachezarLalov@users.noreply.github.com>
2025-09-22 18:50:48 +0300

    Add Express Server

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # movies-catalog-09-2025
+
 Workshop project - SoftUni JS BackEnd
+
+## Development steps
+
+-   [x] Initialize Project
+-   [ ] Add Express Server
+-   [ ] Add Worshop Resources
+-   [ ] Setup Handlebars
+-   [ ] Render Home Page
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('It works!');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('It Works!');
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000...');
+});
