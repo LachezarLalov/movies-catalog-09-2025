a6db41d2cec08e7d0b3f4a953b3570979e84d15c
LachezarLalov <113022668+LachezarLalov@users.noreply.github.com>
2025-09-22 18:58:35 +0300

    Setup Handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+// Setup
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+// Routes
+
 app.get('/', (req, res) => {
-    res.send('It Works!');
+    res.render('home', { layout: false });
 });
 
 app.listen(5000, () => {
